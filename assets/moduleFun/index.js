65c378eb68590088ae70bee61985ddb5e7eae45f
pdcourses <freshitpt@gmail.com>
2022-07-09 10:18:44 +0300

    module import and export

---
+++ assets/moduleFun/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function multOne(value){
+    return value*1;
+}
+
+export function sum(val1, val2){
+    return val1 + val2;
+}
+
+export function sub(val1, val2){
+    return val1 - val2;
+}
+
+export function mult(val1, val2){
+    return val1 * val2;
+}
+
+export function divi(val1, val2){
+    return val1 / val2;
+}
---
+++ assets/moduleMain/index.js
@@ -0,0 +1,9 @@
+// main module
+'use strict';
+
+import {sum, sub, mult, divi } from '../moduleFun/index.js';
+
+console.log(sum(10,20));
+console.log(sub(10,20));
+console.log(mult(10,20));
+console.log(divi(10,20));
